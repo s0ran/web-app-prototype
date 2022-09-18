cf88f01f336dee0657e13c905acc75a1c8afcd2b
s0ran <s0ran.n.11921@gmail.com>
2022-09-18 18:00:28 +0900

    修正

--- src/gin-base.sh
+++ src/gin-base.sh
@@ -5,4 +5,5 @@ cd ${BACKEND_NAME};
 git remote remove origin;
 cd ../;
 eval gsed -e 's/{PROJECT_NAME}/$PROJECT_NAME/g' example/Dockerfile.development > ${BACKEND_NAME}/Dockerfile.development;
-eval gsed -e 's/{BACKEND_NAME}/$BACKEND_NAME/g' example/docker-compose.development.yml | gsed -e 's/{PROJECT_NAME}/$PROJECT_NAME/g'  > ./docker-compose.development.yml;
+eval gsed -e 's/{BACKEND_NAME}/$BACKEND_NAME/g' example/docker-compose.development.yml | eval gsed -e 's/{PROJECT_NAME}/$PROJECT_NAME/g'  > ./docker-compose.development.yml;
+rm -rf example;
